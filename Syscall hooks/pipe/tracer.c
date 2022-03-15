00ec37166498815b2f9e0eb04b301a111ed28a8d
Denis <70904339+denkolochkin@users.noreply.github.com>
2022-03-15 21:13:29 +0700

    Create tracer.c

---
+++ Syscall hooks/pipe/tracer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int error_code;
+    int eax = getchar();
+    int rdi;
+    int tmp;
+    printf("\n");
+    while (EOF != eax) {
+        //rintf("SYSCALL %d\n", eax - '0');
+        rdi = (getchar() - '0') * 100 + (getchar() - '0') * 10 + (getchar() - '0');
+        //printf("close() parameter - %d\n", rdi);
+        error_code = close(rdi);
+        if (0 != error_code) {
+            tmp = 10;
+        }
+        tmp += 10;
+        eax = getchar();
+    }
+    return 0;
+}
